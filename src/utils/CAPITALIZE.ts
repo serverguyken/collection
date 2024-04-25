c9e6f3e8bf66633201798f4d0d8b0da0297ee85e
serverguyken <serverguyken@gmail.com>
2024-04-25 15:27:55 -0400

    feat: add type to function

--- src/utils/CAPITALIZE.ts
+++ src/utils/CAPITALIZE.ts
@@ -3,15 +3,15 @@
  * Capitalize the first letter of a string
  * @param str  The string to capitalize
  */
-const CAPITALIZE = (str: string) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+const CAPITALIZE = <T extends string>(str: T): Capitalize<T> => {
+    return str.charAt(0).toUpperCase() + str.slice(1) as Capitalize<T>;
 }
 /**
  *  Capitalize each word in a string
  * @param str The string to capitalize 
  */
-export function CAPITALIZEEACHWORD(str: string): string {
-    return str.replace(/\b\w/g, l => l.toUpperCase());
+export function CAPITALIZEEACHWORD <T extends string>(str: T): Uppercase<T> {
+    return str.replace(/\b\w/g, l => l.toUpperCase()) as Uppercase<T>;
 }
 
 export default CAPITALIZE;
