0431514547dc26488ad5131ed850342a56c78348
serverguyken <serverguyken@gmail.com>
2024-05-03 00:52:21 -0400

    feat: add IF and IFSTRICT function

--- src/utils/IF.ts
+++ src/utils/IF.ts
@@ -8,3 +8,17 @@ export default function IF<Value extends any>(value: Value): boolean {
     if (value === 0) return true;
     return value ? true : false
 }
+
+
+/**
+ * Returns a boolean value based on the strict equality comparison of
+ * the input `value`. If `value` is strictly equal to 0, it returns `false`. If `value` is strictly
+ * equal to 1, it returns `true`. Otherwise, it returns `true` if `value` is truthy, and `false` if
+ * `value` is falsy.
+ * @param {Value} value - any type.
+ */
+export function IFSTRICT<Value extends any>(value: Value): boolean {
+    if (value === 0) return false;
+    if (value === 1) return true;
+    return value ? true : false
+}
